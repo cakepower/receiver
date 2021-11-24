8bc42b3a0ac4e52679cb1a80d9e260f2b1273f53
cakepower <32028127+cakepower@users.noreply.github.com>
2021-11-25 08:44:42 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,15 +2,6 @@ radio.onReceivedNumber(function (receivedNumber) {
     received_number = receivedNumber
     basic.showNumber(receivedNumber)
     basic.pause(100)
-})
-let counter = 0
-let received_number = 0
-radio.setGroup(33)
-let strip = neopixel.create(DigitalPin.P11, 24, NeoPixelMode.RGB)
-strip.setBrightness(50)
-strip.showColor(neopixel.colors(NeoPixelColors.Black))
-strip.show()
-basic.forever(function () {
     if (received_number == 1) {
         basic.showLeds(`
             . . # . .
@@ -19,10 +10,10 @@ basic.forever(function () {
             # . . . #
             # . . . #
             `)
-        counter = counter + received_number
         strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Red))
         strip.show()
         basic.pause(500)
+        counter = counter + received_number
     } else if (received_number == -1) {
         basic.showLeds(`
             # # # # .
@@ -43,11 +34,22 @@ basic.forever(function () {
             . # # # .
             . . # . .
             `)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
         strip.show()
         basic.pause(500)
         counter = 0
     } else {
     	
     }
+})
+let counter = 0
+let received_number = 0
+let strip: neopixel.Strip = null
+radio.setGroup(33)
+strip = neopixel.create(DigitalPin.P11, 24, NeoPixelMode.RGB)
+strip.setBrightness(50)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+strip.show()
+basic.forever(function () {
+	
 })
